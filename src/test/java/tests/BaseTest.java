3dc0004a521014af447033d6b3b69c0e04e7c211
OrBaruch <109754737+orbaruch1994@users.noreply.github.com>
2024-02-11 09:29:24 +0200

    Update BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -29,7 +29,7 @@ public class BaseTest {
 	
 	@BeforeClass
 	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://automationteststore.com/");
